e71bc4b16fc79f629934e38d7933a6677615c4e8
lashkaydaniel <lashkay74y@gmail.com>
2025-02-28 13:45:15 +0200

    add custom layouts

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheLogo from "@/components/TheLogo.vue";
+</script>
+
+<template>
+  <main class="w-full h-full">
+    <section class="flex flex-col justify-center items-center bg-gray-100/10 w-1/2 mx-auto mt-[10vh] rounded-lg">
+      <the-logo/>
+      <div class="w-full mt-4 rounded-b-lg border-t border-gray-200/10">
+        <slot></slot>
+      </div>
+    </section>
+  </main>
+</template>
