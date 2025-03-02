82f64df585485ffb8f2fe4ede775dc918deef641
lashkaydaniel <lashkay74y@gmail.com>
2025-03-02 17:00:40 +0200

    display product list

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,10 +1,18 @@
 <script setup>
-
 import MainLayout from "@/layouts/MainLayout.vue";
+import ProductList from "@/components/ProductList.vue";
+
+
 </script>
 
 <template>
   <main-layout>
-    <h1 class="text-white">Catalog page</h1>
+    <section class="p-10 flex flex-row items-start space-x-2">
+      <div class="sticky top-2 basis-64 bg-gray-300">
+        filters
+      </div>
+
+      <product-list></product-list>
+    </section>
   </main-layout>
 </template>
