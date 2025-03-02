039fbdba25d3db6aae7ea0b873dd9f4bf015b3db
lashkaydaniel <lashkay74y@gmail.com>
2025-03-02 16:57:37 +0200

    create Pinia state for basket

---
+++ src/stores/basket.js
@@ -0,0 +1,35 @@
+import {defineStore} from "pinia";
+import {computed, reactive, watch} from "vue";
+
+export const useBasketStore = defineStore('basket', () => {
+    // state
+    const basketList = reactive(
+        JSON.parse(localStorage.getItem('basket') || '[]'))
+
+    watch(basketList, (newBasket) => {
+        localStorage.setItem('basket', JSON.stringify(basketList))
+    }, {deep: true})
+
+    // getters
+    const getItems = computed(() => {
+        return basketList
+    })
+
+
+    // actions
+    const isExists = (id) => {
+        return basketList.some(itemId => itemId === id)
+    }
+
+    const addProduct = (product) => {
+        const {id: productId} = product
+        if (isExists(productId)) {
+            return false
+        }
+        basketList.push(productId)
+        return true
+    }
+
+
+    return {getItems, isExists, addProduct}
+})
