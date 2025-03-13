f3af8fda782306081b1219e6d6941ff195bdf56a
lashkaydaniel <lashkay74y@gmail.com>
2025-03-13 21:45:25 +0200

    add filters for product list

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,18 +1,20 @@
 <script setup>
 import MainLayout from "@/layouts/MainLayout.vue";
 import ProductList from "@/components/product/ProductList.vue";
+import FilterOption from "@/components/product/FilterOption.vue";
+import {ref} from "vue";
 
-
+const sortBy = ref('default')
 </script>
 
 <template>
   <main-layout>
-    <section class="p-10 flex flex-row items-start space-x-2">
-      <div class="sticky top-2 basis-64 bg-gray-300">
-        filters
+    <section class="p-10 flex flex-row space-x-6">
+      <div class="sticky top-2 basis-64 text-gray-300">
+        <filter-option v-model:sort-by="sortBy"/>
       </div>
 
-      <product-list></product-list>
+      <product-list :filters="sortBy"/>
     </section>
   </main-layout>
 </template>
