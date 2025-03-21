18db8e45acca101b28790ff5cb1bbe40871a8233
lashkaydaniel <lashkay74y@gmail.com>
2025-03-21 20:27:15 +0200

    add user profile view

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MainLayout from "@/layouts/MainLayout.vue";
+import {useAuthStore} from "@/stores/auth.js";
+import {useRouter} from "vue-router";
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+function logout() {
+  authStore.logout()
+  router.push('/')
+}
+</script>
+
+
+<template>
+  <main-layout>
+
+    <section class="w-1/2 mx-auto bg-gray-500/30 p-4 rounded">
+      <h1 class="text-gray-300 text-3xl mb-4">Profile</h1>
+      <p class="text-amber-400/80">Hi, {{ authStore?.user?.displayName ?? authStore?.user?.email }}</p>
+      <button @click="logout" class="mt-4 bg-amber-500 px-4 py-1 rounded-lg">Logout</button>
+    </section>
+
+  </main-layout>
+</template>
