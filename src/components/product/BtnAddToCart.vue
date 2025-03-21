b6f927a88f8a95c9ed20f7199bd963dbd0d0634f
lashkaydaniel <lashkay74y@gmail.com>
2025-03-21 20:34:48 +0200

    some fix

--- src/components/product/BtnAddToCart.vue
+++ src/components/product/BtnAddToCart.vue
@@ -1,35 +1,37 @@
 <script setup>
 import {computed} from "vue";
 import {useBasketStore} from "@/stores/basket.js";
+import {useAuthStore} from "@/stores/auth.js";
 
 const props = defineProps({
   product: Object
 })
 
 const basketStore = useBasketStore()
+const {isAuthenticated} = useAuthStore()
 
 const isInBasket = computed(() => {
+  if (!isAuthenticated) return false
   return basketStore.isExists(props.product.id)
 })
 
 const btnClassStyle = computed(() => {
   if (isInBasket.value) {
-    return 'bg-green-400 hover:bg-green-600'
+    return 'bg-green-400 hover:bg-green-600 border-green-700'
   }
-  return 'bg-yellow-500 hover:bg-yellow-600'
+  return 'bg-yellow-500 hover:bg-yellow-600 border-orange-400'
 })
 
 
 function addToCart() {
-  const successfully = basketStore.addProduct(props.product)
-  if (!successfully) alert("Item is in the cart")
+  basketStore.addProduct(props.product)
 }
 </script>
 
 <template>
   <button @click="addToCart"
           type="button"
-          class="mt-2 text-black font-bold px-4 py-1 rounded"
+          class="mt-2 text-black font-bold px-4 py-1 rounded border-b-3"
           :class="btnClassStyle">
     {{ isInBasket ? 'In Basket' : 'Add to Cart' }}
   </button>
--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -6,6 +6,7 @@ import TheLogo from "@/components/TheLogo.vue";
   <main class="w-full h-full">
     <section class="flex flex-col justify-center items-center bg-gray-100/10 w-1/2 mx-auto mt-[10vh] rounded-lg">
       <the-logo/>
+      <slot name="title"/>
       <div class="w-full mt-4 rounded-b-lg border-t border-gray-200/10">
         <slot></slot>
       </div>
