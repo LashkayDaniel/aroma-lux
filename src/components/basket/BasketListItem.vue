2ab737755a15190c7e054195738ad5f6d05f7488
lashkaydaniel <lashkay74y@gmail.com>
2025-03-26 20:34:01 +0200

    add responsive design for basket

--- src/components/basket/BasketListItem.vue
+++ src/components/basket/BasketListItem.vue
@@ -20,18 +20,18 @@ const deleteItem = () => {
   <button @click="deleteItem"
           class="group self-center mr-2 hover:bg-red-200 p-1 rounded" title="Delete">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-         class="size-6 stroke-gray-400 group-hover:stroke-red-400">
+         class="size-5 sm:size-6 stroke-gray-400 group-hover:stroke-red-400">
       <path stroke-linecap="round" stroke-linejoin="round"
             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
     </svg>
   </button>
 
-  <img :src="item?.image" alt="product" class="size-20 rounded">
+  <img :src="item?.image" alt="product" class="size-20 rounded object-cover h-full">
   <div class="flex-1 ml-2 flex justify-between">
     <div class="">
-      <h2 class="text-semibold text-lg">{{ fullTitleName }}</h2>
+      <h2 class="text-semibold text-sm sm:text-base md:text-lg">{{ fullTitleName }}</h2>
       <p class="text-gray-400 text-sm">{{ item?.category }}</p>
     </div>
-    <b class="ml-10 px-1 text-gray-700 text-lg">${{ item?.price }}</b>
+    <b class="ml-6 sm:ml-10 px-1 text-gray-700 text-base sm:text-lg">${{ item?.price }}</b>
   </div>
 </template>
--- src/components/basket/BasketSidebar.vue
+++ src/components/basket/BasketSidebar.vue
@@ -27,13 +27,13 @@ function closeHandler() {
     <aside
         class="bg-white absolute right-0 h-full p-4 min-w-1/4 z-20 border-l-2 border-gray-400 flex flex-col justify-between">
       <div class="flex justify-between items-center border-b border-gray-200 py-2">
-        <h1 class="text-2xl font-bold">Basket</h1>
+        <h1 class="text-xl md:text-2xl font-bold">Basket</h1>
         <button @click="closeHandler"
                 class="bg-gray-200 p-1 rounded hover:bg-gray-300 transition-colors duration-200"
                 title="Close">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                stroke="currentColor"
-               class="size-6 stroke-gray-500">
+               class="size-5 sm:size-6 stroke-gray-500">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
           </svg>
         </button>
@@ -43,7 +43,8 @@ function closeHandler() {
 
       <div v-if="totalSum>0"
            class="mt-4 p-2 border-t border-gray-300 bg-linear-to-r from-white to-amber-300 rounded text-amber-700 text-end">
-        <h2 class="text-xl font-semibold mr-2">Total: <span class="ml-2 font-bold">${{ totalSum }}</span></h2>
+        <h2 class="text-base sm:text-xl font-semibold mr-2">Total: <span class="ml-2 font-bold">${{ totalSum }}</span>
+        </h2>
       </div>
     </aside>
   </div>
