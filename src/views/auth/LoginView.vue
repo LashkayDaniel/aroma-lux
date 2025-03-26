1a81a6cdf165c4789838780453ee9edca18184fa
lashkaydaniel <lashkay74y@gmail.com>
2025-03-26 20:53:30 +0200

    add responsive design for auth

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -46,48 +46,48 @@ const vFocus = {
 <template>
   <auth-layout>
     <template #title>
-      <h1 class="text-2xl text-zinc-400 mt-2 font-bold">Log In</h1>
+      <h1 class="text-lg sm:text-xl md:text-2xl text-zinc-400 mt-2 font-bold">Log In</h1>
     </template>
 
-    <form @submit.prevent="onFormSubmit" class="flex flex-col my-5 mx-auto w-1/2">
-      <label for="email" class="text-lg font-semibold text-gray-300">Email:</label>
+    <form @submit.prevent="onFormSubmit" class="flex flex-col my-5 mx-auto w-[90%] md:w-1/2">
+      <label for="email" class="text-base sm:text-lg font-semibold text-gray-300">Email:</label>
       <input v-focus
              v-model="email"
              v-bind="emailAttrs"
              id="email"
              :class="{'border border-red-500/60':errors.email}"
-             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
+             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 text-sm sm:text-base focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
              type="text"
              placeholder="darkmark@gmail.com">
       <show-animation>
-        <p class="text-sm text-red-500" v-if="errors.email">{{ errors.email }}</p>
+        <p class="text-xs sm:text-sm mt-1 text-red-500" v-if="errors.email">{{ errors.email }}</p>
       </show-animation>
 
-      <label for="password" class="text-lg mt-2 font-semibold text-gray-300">Password:</label>
+      <label for="password" class="text-base sm:text-lg mt-2 font-semibold text-gray-300">Password:</label>
       <input v-model="password"
              v-bind="passwordAttrs"
              id="password"
              :class="{'border border-red-500/60':errors.password}"
-             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
+             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 text-sm sm:text-base focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
              type="password"
              placeholder="input password">
       <show-animation>
-        <p class="text-sm text-red-500" v-if="errors.password">{{ errors.password }}</p>
+        <p class="text-xs sm:text-sm mt-1 text-red-500" v-if="errors.password">{{ errors.password }}</p>
       </show-animation>
 
       <show-animation>
-        <p class="text-sm text-red-400 mt-3" v-if="authError">{{ authError }}</p>
+        <p class="text-xs sm:text-sm text-red-400 mt-3" v-if="authError">{{ authError }}</p>
       </show-animation>
 
       <div class="flex items-end justify-between">
         <button type="submit"
                 :disabled="!meta.valid"
-                class="w-fit py-1 px-4 mt-4 rounded-lg border-b-2 border-amber-700 text-black font-semibold bg-amber-500 hover:bg-amber-600/90 transition-colors duration-400 disabled:bg-amber-500/30">
+                class="w-fit py-1 px-4 mt-4 text-sm sm:text-base rounded-lg border-b-2 border-amber-700 text-black font-semibold bg-amber-500 hover:bg-amber-600/90 transition-colors duration-400 disabled:bg-amber-500/30">
           Login
         </button>
 
         <router-link to="/register"
-                     class="text-amber-500/80 hover:text-amber-500/60 transition-all duration-300">
+                     class="text-amber-500/80 text-sm sm:text-base hover:text-amber-500/60 transition-all duration-300">
           Register
         </router-link>
       </div>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -47,61 +47,61 @@ const vFocus = {
 <template>
   <auth-layout>
     <template #title>
-      <h1 class="text-2xl text-zinc-400 mt-2 font-bold">Register</h1>
+      <h1 class="text-lg sm:text-xl md:text-2xl text-zinc-400 mt-2 font-bold">Register</h1>
     </template>
 
-    <form @submit.prevent="onFormSubmit" class="flex flex-col my-5 mx-auto w-1/2 relative">
-      <label for="name" class="text-lg font-semibold text-gray-300">Name:</label>
+    <form @submit.prevent="onFormSubmit" class="flex flex-col my-5 mx-auto w-[90%] md:w-1/2">
+      <label for="name" class="text-base sm:text-lg font-semibold text-gray-300">Name:</label>
       <input v-focus
              v-model="name"
              v-bind="nameAttrs"
              id="name"
              :class="{'border border-red-500/60':errors.name}"
-             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
+             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 text-sm sm:text-base focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
              type="text"
              placeholder="Mark">
       <show-animation>
-        <p class="text-sm text-red-500" v-if="errors.name">{{ errors.name }}</p>
+        <p class="text-xs sm:text-sm mt-1 text-red-500" v-if="errors.name">{{ errors.name }}</p>
       </show-animation>
 
-      <label for="email" class="text-lg font-semibold text-gray-300">Email:</label>
+      <label for="email" class="text-base sm:text-lg mt-2 font-semibold text-gray-300">Email:</label>
       <input v-focus
              v-model="email"
              v-bind="emailAttrs"
              id="email"
              :class="{'border border-red-500/60':errors.email}"
-             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
+             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 text-sm sm:text-base focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
              type="text"
              placeholder="darkmark@gmail.com">
       <show-animation>
-        <p class="text-sm text-red-500" v-if="errors.email">{{ errors.email }}</p>
+        <p class="text-xs sm:text-sm mt-1 text-red-500" v-if="errors.email">{{ errors.email }}</p>
       </show-animation>
 
-      <label for="password" class="text-lg mt-2 font-semibold text-gray-300">Password:</label>
+      <label for="password" class="text-base sm:text-lg mt-2 font-semibold text-gray-300">Password:</label>
       <input v-model="password"
              v-bind="passwordAttrs"
              id="password"
              :class="{'border border-red-500/60':errors.password}"
-             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
+             class="bg-zinc-500/50 px-2 py-1 rounded text-amber-400 text-sm sm:text-base focus:ring-amber-500/50 focus:ring-1 focus:outline-none"
              type="password"
              placeholder="input password">
       <show-animation>
-        <p class="text-sm text-red-500" v-if="errors.password">{{ errors.password }}</p>
+        <p class="text-xs sm:text-sm mt-1 text-red-500" v-if="errors.password">{{ errors.password }}</p>
       </show-animation>
 
       <show-animation>
-        <p class="text-sm text-red-400 mt-3" v-if="authError">{{ authError }}</p>
+        <p class="text-xs sm:text-sm text-red-400 mt-3" v-if="authError">{{ authError }}</p>
       </show-animation>
 
       <div class="flex items-end justify-between">
         <button type="submit"
                 :disabled="!meta.valid"
-                class="w-fit py-1 px-4 mt-4 rounded-lg border-b-2 border-amber-700 text-black font-semibold bg-amber-500 hover:bg-amber-600/90 transition-colors duration-400 disabled:bg-amber-500/30">
+                class="w-fit py-1 px-4 mt-4 text-sm sm:text-base rounded-lg border-b-2 border-amber-700 text-black font-semibold bg-amber-500 hover:bg-amber-600/90 transition-colors duration-400 disabled:bg-amber-500/30">
           Register
         </button>
 
         <router-link to="/login"
-                     class="text-amber-500/80 hover:text-amber-500/60 transition-all duration-300">
+                     class="text-amber-500/80 text-sm sm:text-base hover:text-amber-500/60 transition-all duration-300">
           Login
         </router-link>
       </div>
